09028c8fc577618cf225e08989b3135f0dd92199
Reem Khaled <roooka219@gmail.com>
2026-08-21 08:58:55 +0300

    Initial project structure

---
+++ src/main/java/com/mentorship/hanakoleh/HanakolehApplication.java
@@ -0,0 +1,13 @@
+package com.mentorship.hanakoleh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanakolehApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanakolehApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mentorship/hanakoleh/HanakolehApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mentorship.hanakoleh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanakolehApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
