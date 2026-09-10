4d4d65a7c35b24efeb389ea78182b486017036c7
zesuliman <zeinab_so@hotmail.com>
2026-09-10 08:55:24 +0200

    added basic skeleton for OrderController,OrderService, OrderRepository, OrderStatusUpdateEvent, OrderStatusUpdateService, and UpdateOrderStatusRequest

---
+++ src/main/java/com/mentorship/hanakoleh/domain/order/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.mentorship.hanakoleh.domain.order.repository;
+
+import com.mentorship.hanakoleh.domain.order.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+}
