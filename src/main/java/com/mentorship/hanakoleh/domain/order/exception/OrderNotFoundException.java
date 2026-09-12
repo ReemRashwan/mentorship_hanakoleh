d69a82249d030bc5618624a722b4155c05c13fa5
Hajar Ramadan <hajar.ramadan777@gmail.com>
2026-09-12 08:59:04 +0300

    Order Subsystem : added Order exception

---
+++ src/main/java/com/mentorship/hanakoleh/domain/order/exception/OrderNotFoundException.java
@@ -0,0 +1,8 @@
+package com.mentorship.hanakoleh.domain.order.exception;
+
+public class OrderNotFoundException extends RuntimeException {
+
+    public OrderNotFoundException(Long orderId) {
+        super("Order not found: " + orderId);
+    }
+}
