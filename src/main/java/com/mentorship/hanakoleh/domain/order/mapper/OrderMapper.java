603ff3d320e3c7505d143ad6cd164ae9905cb221
Reem Khaled <roooka219@gmail.com>
2026-09-12 09:37:30 +0300

    Fix OrderMapper that was broken during merge.

--- src/main/java/com/mentorship/hanakoleh/domain/order/mapper/OrderMapper.java
+++ src/main/java/com/mentorship/hanakoleh/domain/order/mapper/OrderMapper.java
@@ -7,15 +7,14 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
 
-
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
-    @Mapping(source = "id", target = "orderId")
-    @Mapping(source = "restaurant.name", target = "restaurantName")
-    @Mapping(source = "finalStatus", target = "status")
-    @Mapping(source = "totalAmount", target = "totalAmount")
-    @Mapping(source = "currencyCode", target = "currencyCode")
+    @Mapping(source = "order.id", target = "orderId")
+    @Mapping(source = "order.restaurant.name", target = "restaurantName")
+    @Mapping(source = "order.finalStatus", target = "status")
+    @Mapping(source = "order.totalAmount", target = "totalAmount")
+    @Mapping(source = "order.currencyCode", target = "currencyCode")
     @Mapping(source = "itemLineCount", target = "itemLineCount")
-    @Mapping(source = "createdAt", target = "createdAt")
+    @Mapping(source = "order.createdAt", target = "createdAt")
     OrderHistoryResponse toOrderHistoryResponse(Order order, long itemLineCount);
 }
