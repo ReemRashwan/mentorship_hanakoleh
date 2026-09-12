6e0de4de2d61d82f81f063c37794161cbe7fa9be
Reem Khaled <roooka219@gmail.com>
2026-09-12 07:43:07 +0300

    Fix OrderMapper.

--- src/main/java/com/mentorship/hanakoleh/domain/order/mapper/OrderMapper.java
+++ src/main/java/com/mentorship/hanakoleh/domain/order/mapper/OrderMapper.java
@@ -10,12 +10,12 @@ import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface OrderMapper {
-    @Mapping(source = "order.id", target = "orderId")
-    @Mapping(source = "order.restaurant.name", target = "restaurantName")
-    @Mapping(source = "order.finalStatus", target = "status")
-    @Mapping(source = "order.totalAmount", target = "totalAmount")
-    @Mapping(source = "order.currencyCode", target = "currencyCode")
+    @Mapping(source = "id", target = "orderId")
+    @Mapping(source = "restaurant.name", target = "restaurantName")
+    @Mapping(source = "finalStatus", target = "status")
+    @Mapping(source = "totalAmount", target = "totalAmount")
+    @Mapping(source = "currencyCode", target = "currencyCode")
     @Mapping(source = "itemLineCount", target = "itemLineCount")
-    @Mapping(source = "order.createdAt", target = "createdAt")
+    @Mapping(source = "createdAt", target = "createdAt")
     OrderHistoryResponse toOrderHistoryResponse(Order order, long itemLineCount);
 }
